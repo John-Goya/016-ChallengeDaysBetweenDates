717780c4f42dc67491de6e08ad09269448574e40
goyaschooling <goyaschooling@gmail.com>
2017-09-07 22:44:26 -0700

    Add All Files

---
+++ ChallengeDaysBetweenDates/Default_DaysBetweenDates.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default_DaysBetweenDates.aspx.cs" Inherits="ChallengeDaysBetweenDates.Default_DaysBetweenDates" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Choose one date:
+            <asp:Label ID="selectedFirstDateLabel" runat="server"></asp:Label>
+            <asp:Calendar ID="firstCalendar" runat="server" OnSelectionChanged="firstCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <br />
+            Choose a second date:
+            <asp:Label ID="selectedSecondDateLabel" runat="server"></asp:Label>
+            <asp:Calendar ID="secondCalendar" runat="server" OnSelectionChanged="secondCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="calculateButton" runat="server" Text="Ok" OnClick="calculateButton_Click" />
+            <br />
+            <br />
+            Number of days between selected dates : <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/Default_DaysBetweenDates.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default_DaysBetweenDates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void firstCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime firstDate = firstCalendar.SelectedDate;
+            selectedFirstDateLabel.Text = firstDate.ToShortDateString();
+        }
+
+        protected void secondCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime secondDate = secondCalendar.SelectedDate;
+            selectedSecondDateLabel.Text = secondDate.ToShortDateString();
+        }
+
+        protected void calculateButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstDate = firstCalendar.SelectedDate.Date;
+            DateTime secondDate = secondCalendar.SelectedDate.Date;
+
+            //TimeSpan timeDifference = secondDate.Subtract(firstDate);
+            TimeSpan timeDifference = (secondDate - firstDate).Duration();
+            //resultLabel.Text = timeDifference.ToString();
+            resultLabel.Text = timeDifference.TotalDays.ToString();
+
+
+            //TimeSpan timeDifference = new TimeSpan();
+            //timeDifference = selectedFirstDateLabel - selectedSecondDateLabel;
+            //resultLabel.Text = 
+        }
+    }
+}
